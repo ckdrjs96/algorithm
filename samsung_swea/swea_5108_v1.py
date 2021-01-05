87396163637c44857959339c6cabc1f07287e716
ckdrjs <ckdrjs96@gmail.com>
2021-01-06 00:36:03 +0900

    5108

---
+++ samsung_swea/swea_5108_v1.py
@@ -0,0 +1,12 @@
+T = int(input())
+
+for test_case in range(1, T + 1):
+    N, M, L = map(int, input().split())
+    array = list(map(int, input().split()))
+
+    for _ in range(M):
+        index, num = map(int, input().split())
+        array = array[:index] + [num] + array[index:]
+        # array.insert(*map(int, input().split()))
+
+    print(f'#{test_case} {array[L]}')
