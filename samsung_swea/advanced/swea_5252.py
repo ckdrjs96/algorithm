ccaa441828b0c6be29aa8f6dcc9ec7b7f4609922
ckdrjs <ckdrjs96@gmail.com>
2021-02-23 00:56:57 +0900

    string search

---
+++ samsung_swea/advanced/swea_5252.py
@@ -0,0 +1,7 @@
+#T=1
+T =int(input())
+for test_case in range(1, T + 1):
+    N, M = map(int, input().split())
+    A=[input() for _ in range(N)]
+    B = [input() for _ in range(M)]
+    print(f'#{test_case}',len(set(A)&set(B)))
