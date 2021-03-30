7059348836b66e810266bfcdb51038e326b47436
ckdjrs96 <ckdrjs96@gmail.com>
2021-03-31 00:32:59 +0900

    scofe2021

--- CodingTest/SCOFE 2021/round2/test4.py
+++ CodingTest/SCOFE 2021/round2/test4.py
@@ -5,7 +5,7 @@ N=int(input().rstrip())
 indexs=[input().rstrip() for _ in range(N)]
 Q=int(input().rstrip())
 searches=[input().rstrip() for _ in range(Q)]
-print(searches)
+
 for search in searches:
     cnt=0
     for index in indexs:
